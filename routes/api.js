b8ad56e9af099cf8f9cc0fcb1cf77d024aa0a97e
maxam2017 <whk0313@gmail.com>
2018-08-15 11:14:04 +0800

    init

---
+++ routes/api.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql')
+var dbconfig = require('../db/config')
+var sql = require('../db/sql')
+var newcs_pool = mysql.createPool(dbconfig.newcs)
+var oldexam_pool = mysql.createPool(dbconfig.oldexam)
+
+router.get('/students',function(req,res,next){
+  newcs_pool.getConnection(function(err,connection){
+      connection.query(sql.getStudent,function(err,result){
+        res.json(result)
+        connection.release()
+      })
+    }
+  )
+})
+
+
+router.post('/pay',function(req,res,next){
+  newcs_pool.getConnection(function(err,connection){
+      console.log(req.body)
+      connection.query(sql.payFee,[req.body.id],function(err,result){
+        if(err){
+          res.json({success:false})
+        }
+        else{
+          res.json({id:req.body.id,success:(result.changedRows === 1)})
+        }
+        connection.release()
+      })
+    }
+  )
+})
+
+router.get('/course',function(req,res,next){
+  oldexam_pool.getConnection(function(err,connection){
+      connection.query(sql.getCourse,function(err,result){
+        res.json(result)
+        connection.release()
+      })
+    }
+  )
+})
+
+router.get('/exam',function(req,res,next){
+  oldexam_pool.getConnection(function(err,connection){
+    var param = req.query || req.params
+    connection.query(sql.getList,[param.id],function(err,result){
+        res.json(result)
+        connection.release()
+      })
+    }
+  )
+})
+
+module.exports = router;
