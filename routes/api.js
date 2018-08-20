33bb05134e41a8dd55fc2de1b903b988e4adaf5d
maxam2017 <whk0313@gmail.com>
2018-08-20 19:37:32 +0800

    port

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,11 @@ var router = express.Router();
 var mysql = require('mysql')
 var dbconfig = require('../db/config')
 var sql = require('../db/sql')
-var newcs_pool = mysql.createPool(dbconfig.newcs)
-var oldexam_pool = mysql.createPool(dbconfig.oldexam)
+var cs_pool = mysql.createPool(dbconfig.csunion)
+//var oldexam_pool = mysql.createPool(dbconfig.oldexam)
 
 router.get('/students',function(req,res,next){
-  newcs_pool.getConnection(function(err,connection){
+  cs_pool.getConnection(function(err,connection){
       connection.query(sql.getStudent,function(err,result){
         res.json(result)
         connection.release()
@@ -19,7 +19,7 @@ router.get('/students',function(req,res,next){
 
 
 router.post('/pay',function(req,res,next){
-  newcs_pool.getConnection(function(err,connection){
+  cs_pool.getConnection(function(err,connection){
       console.log(req.body)
       connection.query(sql.payFee,[req.body.id],function(err,result){
         if(err){
@@ -34,25 +34,25 @@ router.post('/pay',function(req,res,next){
   )
 })
 
-router.get('/course',function(req,res,next){
-  oldexam_pool.getConnection(function(err,connection){
-      connection.query(sql.getCourse,function(err,result){
-        res.json(result)
-        connection.release()
-      })
-    }
-  )
-})
+// router.get('/course',function(req,res,next){
+//   oldexam_pool.getConnection(function(err,connection){
+//       connection.query(sql.getCourse,function(err,result){
+//         res.json(result)
+//         connection.release()
+//       })
+//     }
+//   )
+// })
 
-router.get('/exam',function(req,res,next){
-  oldexam_pool.getConnection(function(err,connection){
-    var param = req.query || req.params
-    connection.query(sql.getList,[param.id],function(err,result){
-        res.json(result)
-        connection.release()
-      })
-    }
-  )
-})
+// router.get('/exam',function(req,res,next){
+//   oldexam_pool.getConnection(function(err,connection){
+//     var param = req.query || req.params
+//     connection.query(sql.getList,[param.id],function(err,result){
+//         res.json(result)
+//         connection.release()
+//       })
+//     }
+//   )
+// })
 
 module.exports = router;
